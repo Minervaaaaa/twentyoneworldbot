14e91ac409b4293b147b85c346a02313bdac8c39
MinervaCL <92528954+Minervaaaaa@users.noreply.github.com>
2023-01-13 06:55:17 +0100

    Rename
    Rename Einundzwanzig to Twentyonebot

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/user/Documents/GitHub/einundzwanzigbot/env"
+set -gx VIRTUAL_ENV "/Users/user/Documents/GitHub/twentyoneworldbot/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
